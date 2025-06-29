6fbeda2acd480ace5e97a88889659866784260d5
Dagim <dagimed6@gmail.com>
2025-06-29 16:57:57 +0400

    fix task 4 and 6

--- task_4.sql
+++ task_4.sql
@@ -1,12 +1,3 @@
--- Script to print full description of books table
--- This script will be executed with database name as argument
-SELECT 
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE TABLE_SCHEMA = DATABASE() 
-AND TABLE_NAME = 'books';
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
