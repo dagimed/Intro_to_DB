c6cfc3dda758068567d706034645d3c19b0ff5e0
Dagim <dagimed6@gmail.com>
2025-06-29 17:01:28 +0400

    fix task 4

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,12 @@
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+-- Script to print full description of books table
+-- This script will be executed with database name as argument
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books';
